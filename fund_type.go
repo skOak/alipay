ffd4256e2754025fecb6f62c8280687597b1ee60
克里斯 <lvzhigang@innotechx.com>
2020-07-09 14:51:33 +0800

    FundTransfer

--- fund_type.go
+++ fund_type.go
@@ -105,3 +105,66 @@ func (this *AliPayFundTransOrderQueryResponse) IsSuccess() bool {
 	}
 	return false
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// https://opendocs.alipay.com/apis/api_28/alipay.fund.trans.uni.transfer/
+// 单笔转账接口（新）请求结构
+type TransferParticipant struct {
+	Identity     string `json:"identity"`      // 必填 参与方的唯一标识
+	IdentityType string `json:"identity_type"` // 必填 参与方的标识类型，目前支持如下类型：1、ALIPAY_USER_ID 支付宝的会员ID 2、ALIPAY_LOGON_ID：支付宝登录号，支持邮箱和手机号格式
+	Name         string `json:"name"`          // 可选 参与方真实姓名，如果非空，将校验收款支付宝账号姓名一致性。当identity_type=ALIPAY_LOGON_ID时，本字段必填。
+}
+type FundTransUniTransfer struct {
+	AppAuthToken    string               `json:"-"`                 // 可选
+	OutBizNo        string               `json:"out_biz_no"`        // 必选 商户转账唯一订单号
+	TransAmount     string               `json:"trans_amount"`      // 必选 订单总金额，单位为元
+	ProductCode     string               `json:"product_code"`      // 必选 业务产品码，单笔无密转账到支付宝账户固定为: TRANS_ACCOUNT_NO_PWD； 单笔无密转账到银行卡固定为: RANS_BANKCARD_NO_PWD; 发现金红包固定为: TD_RED_PACKET；
+	BizScene        string               `json:"biz_scene"`         // 可选 描述特定的业务场景,DIRECT_TRANSFER：单笔无密转账到支付宝/银行卡, B2C现金红包;PERSONAL_COLLECTION：C2C现金红包-领红包
+	OrderTitle      string               `json:"order_title"`       // 可选 转账业务的标题，用于在支付宝用户的账单里显示
+	OriginalOrderId string               `json:"original_order_id"` // 可选 原支付宝业务单号。C2C现金红包-红包领取时，传红包支付时返回的支付宝单号；B2C现金红包、单笔无密转账到支付宝/银行卡不需要该参数。
+	PayeeInfo       *TransferParticipant `json:"payee_info"`        // 必选 收款方信息
+	Remark          string               `json:"remark"`            // 可选 业务备注
+	BusinessParams  string               `json:"business_params"`   // 可选 转账业务请求的扩展参数，支持传入的扩展参数如下： 1、sub_biz_scene 子业务场景，红包业务必传，取值REDPACKET，C2C现金红包、B2C现金红包均需传入；2、withdraw_timeliness为转账到银行卡的预期到账时间，可选（不传入则默认为T1），取值T0表示预期T+0到账，取值T1表示预期T+1到账，因到账时效受银行机构处理影响，支付宝无法保证一定是T0或者T1到账；
+
+}
+
+func (this FundTransUniTransfer) APIName() string {
+	return "alipay.fund.trans.uni.transfer"
+}
+
+func (this FundTransUniTransfer) Params() map[string]string {
+	var m = make(map[string]string)
+	m["app_auth_token"] = this.AppAuthToken
+	return m
+}
+
+func (this FundTransUniTransfer) ExtJSONParamName() string {
+	return "biz_content"
+}
+
+func (this FundTransUniTransfer) ExtJSONParamValue() string {
+	return marshal(this)
+}
+
+// 单笔转账接口(新)响应参数
+type FundTransUniTransferResponse struct {
+	Body struct {
+		Code           string `json:"code"`
+		Msg            string `json:"msg"`
+		SubCode        string `json:"sub_code"`
+		SubMsg         string `json:"sub_msg"`
+		OutBizNo       string `json:"out_biz_no"`        // 商户转账唯一订单号：发起转账来源方定义的转账单据号。请求时对应的参数，原样返回
+		OrderId        string `json:"order_id"`          // 支付宝转账单据号，成功一定返回，失败可能不返回也可能返回
+		PayFundOrderId string `json:"pay_fund_order_id"` // 支付宝支付资金流水号
+		Status         string `json:"status"`            // 转账单据状态。
+		TransDate      string `json:"trans_date"`        // 订单支付时间
+	} `json:"alipay_fund_trans_uni_transfer_response"`
+	Sign string `json:"sign"`
+}
+
+func (this *FundTransUniTransferResponse) IsSuccess() bool {
+	if this.Body.Code == K_SUCCESS_CODE {
+		return true
+	}
+	return false
+}
